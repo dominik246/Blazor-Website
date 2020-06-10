8c86abe8f603c7f24efbe7e521b6db722ce05415
Dominik Jelicic <dominik246@gmail.com>
2020-06-10 15:43:43 +0200

    Moved grid creation to JS.

---
+++ LandingWebsite/wwwroot/javascript/AlgoGridCreation.js
@@ -0,0 +1,32 @@
+﻿function CreateGrid(objRef) {
+    let x = 49;
+    let y = 25;
+
+    for (i = 0; i < x; i++) {
+        let container = document.getElementsByClassName("grid_div")[0];
+
+        let divRow = document.createElement("div");
+        divRow.className = "container_div";
+        container.appendChild(divRow);
+
+        for (j = 0; j < y; j++) {
+            let id = `${j}, ${i}`;
+            let unitDiv = document.createElement("div");
+            unitDiv.className = "grid_unit";
+            unitDiv.id = id;
+            unitDiv.onmouseover = function (args) { OnMouseOver(args, objRef, id) };
+            unitDiv.onclick = function () { OnMouseClick(objRef, id) };
+            container.children[i].appendChild(unitDiv);
+        }
+    }
+}
+
+function OnMouseOver(args, obj, id) {
+    if (args.buttons === 1) {
+        obj.invokeMethodAsync("UnitClicked", id);
+    }
+}
+
+function OnMouseClick(obj, id) {
+    obj.invokeMethodAsync("UnitClicked", id);
+}
--- LandingWebsite/wwwroot/javascript/DropDownMenu.js
+++ LandingWebsite/wwwroot/javascript/DropDownMenu.js
@@ -17,7 +17,3 @@ window.onclick = function (event) {
         }
     }
 }
-
-function OnDropdownMenuClick() {
-
-}
