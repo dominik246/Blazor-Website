d1b0e2292a7ea8f2ccf4ae75f0a74c2d5d4bb15a
Dominik Jelicic <dominik246@gmail.com>
2020-06-11 03:28:31 +0200

    Added 2 tabs to Index.

---
+++ LandingWebsite/wwwroot/javascript/DisablingBox.js
@@ -0,0 +1,10 @@
+﻿function Disable() {
+    let unitBoxes = document.getElementsByClassName("sub_box disabledBox");
+    if (unitBoxes[0] === null) {
+        return;
+    }
+    for (let item of unitBoxes) {
+        item.style.setProperty("cursor", "not-allowed");
+        // bug, doesn't show the cursor as "not-allowed"
+    };
+}
